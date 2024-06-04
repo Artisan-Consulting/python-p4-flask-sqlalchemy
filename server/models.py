588f440401b1b4c41ccd58f0725687890c21d783
Artisan <william.kwan@artisanconsultin.nz>
2024-06-04 20:06:23 +1000

    Created app.py models.py

--- server/models.py
+++ server/models.py
@@ -1,8 +1,35 @@
+# server/models.py
+
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
+from enum import unique
 
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
 })
 
 db = SQLAlchemy(metadata=metadata)
+
+class Owner(db.Model):
+    __tablename__ = 'owners'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), unique=True, nullable=False)
+
+    pets = db.relationship('Pet', backref='owner')
+
+    def __repr__(selfself):
+        return f'<Owner {self.name}>'
+
+
+class Pet(db.Model):
+    __tablename__ = 'pets'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    species = db.Column(db.String)
+
+    owner_id = db.Column(db.Integer, db.ForeignKey('owners.id'))
+
+    def __repr__(self):
+        return f'<Pet {self.name}, {self.species}>'
